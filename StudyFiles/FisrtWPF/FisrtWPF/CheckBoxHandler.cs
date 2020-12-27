d5974b96f1fe97455f4c2d63b070b1a921c68f55
Gareth Chen <cxy329288574@gmail.com>
2020-12-27 17:16:22 -0500

    update

---
+++ StudyFiles/FisrtWPF/FisrtWPF/CheckBoxHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FisrtWPF
+{
+    class CheckBoxHandler
+    {
+        public CheckBoxHandler() { }
+
+        public void CheckBoxChangedHelper(ref string text, ref bool isReadOnly, ref Brush color) 
+        {
+            if (color.Equals(Brushes.White))
+            {
+                color = (Brush)new SolidColorBrush(Color.FromRgb(0xee, 0xee, 0xee));
+                isReadOnly = true;
+                text = "";
+            }
+            else 
+            {
+                color = Brushes.White;
+                isReadOnly = false;
+            }
+        }
+    }
+}
---
+++ StudyFiles/FisrtWPF/FisrtWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FisrtWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CheckBoxHandler cbHandler = new CheckBoxHandler();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void amzCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            bool isReadOnly = amzTextBox.IsReadOnly;
+            Brush color = amzTextBox.Background;
+            string text = amzTextBox.Text;
+            cbHandler.CheckBoxChangedHelper(ref text, ref isReadOnly, ref color);
+            amzTextBox.IsReadOnly = isReadOnly;
+            amzTextBox.Background = color;
+            amzTextBox.Text = text;
+        }
+
+        private void dfCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            dfTextBox.IsReadOnly = !dfTextBox.IsReadOnly;
+        }
+
+        private void wfCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            wfTextBox.IsReadOnly = !wfTextBox.IsReadOnly;
+        }
+
+        private void amzpCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            amzpTextBox.IsReadOnly = !amzpTextBox.IsReadOnly;
+        }
+
+        private void wmCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            wmTextBox.IsReadOnly = !wmTextBox.IsReadOnly;
+        }
+
+        private void zxCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            zxTextBox.IsReadOnly = !zxTextBox.IsReadOnly;
+        }
+    }
+}
